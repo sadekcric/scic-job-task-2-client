25c2b434169ff6a17699f5e420c359810bbe1946
sadekcric4 <sadekcric4@gmail.com>
2024-08-16 02:19:42 +0600

    sort search implementation done

--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -9,6 +9,9 @@ const Products = () => {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  // Sorting
+  const [sortByDate, setSortByDate] = useState("asc");
+  const [sortByPrice, setSortByPrice] = useState("asc");
 
   // Pagination
   const [productLength, setProductLength] = useState(0);
@@ -31,13 +34,13 @@ const Products = () => {
   };
 
   useEffect(() => {
-    fetch(`http://localhost:5000/products?pages=${pageNumber}&size=${perPageData}`)
+    fetch(`http://localhost:5000/products?pages=${pageNumber}&size=${perPageData}&sortPrice=${sortByPrice}&sortDate=${sortByDate}`)
       .then((res) => res.json())
       .then((data) => {
         setProducts(data.allProduct);
         setProductLength(data.allProductLength);
       });
-  }, [pageNumber]);
+  }, [pageNumber, sortByPrice, sortByDate]);
 
   // Pagination End
 
@@ -61,27 +64,84 @@ const Products = () => {
     }
   };
 
+  // Category
+  const [category, setCategory] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/search-category?category=${category}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [category]);
+
   return (
     <>
-      {/* Searching */}
+      <div className="lg:w-4/5 mx-auto mt-5 lg:mt-10 overflow-auto">
+        <div className="flex items-center justify-between ">
+          {/* Categorization */}
+          <div className="rounded-full bg-gray-50  shadow-sm shadow-gray-400 inline-block py-3 px-6 ">
+            <select value={category} onChange={(e) => setCategory(e.target.value)} className=" focus:outline-none bg-gray-50 ">
+              <option value="" disabled>
+                Category
+              </option>
+              <option value="Wearables">Wearables</option>
+              <option value="Accessories">Accessories</option>
+              <option value="Footwear">Footwear</option>
+              <option value="Furniture">Furniture</option>
+              <option value="Fitness">Fitness</option>
+              <option value="Home Security">Home Security</option>
+              <option value="Home Decor">Home Decor</option>
+              <option value="Outdoors">Outdoors</option>
+              <option value="Personal Care">Personal Care</option>
+              <option value="Electronics">Electronics</option>
+              <option value="Home Appliances">Home Appliances</option>
+            </select>
+          </div>
 
-      <div className="text-center mt-5 lg:mt-10 ">
-        <div className="rounded-full bg-gray-50  shadow-sm shadow-gray-400 inline-block py-3 px-6 ">
-          <div className="flex items-center justify-between ">
-            <input
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              type="text"
-              placeholder="Search Hear"
-              className="bg-gray-50 focus:outline-none"
-            />
-            <button onClick={handleSearch}>
-              <FaSearch />
-            </button>
+          {/* Searching */}
+
+          <div>
+            <div className="rounded-full bg-gray-50  shadow-sm shadow-gray-400 inline-block py-3 px-6 ">
+              <div className="flex items-center justify-between ">
+                <input
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  type="text"
+                  placeholder="Search Hear"
+                  className="bg-gray-50 focus:outline-none"
+                />
+                <button onClick={handleSearch}>
+                  <FaSearch />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* Sorting */}
+          <div>
+            {/* Sort By Date */}
+            <div className="rounded-full bg-gray-50  shadow-sm shadow-gray-400 inline-block py-3 px-6 mr-5">
+              <select className="bg-gray-50 focus:outline-none" value={sortByDate} onChange={(e) => setSortByDate(e.target.value)}>
+                <option value="" disabled>
+                  Sort By Date
+                </option>
+                <option value="asc">Recent to Past</option>
+                <option value="dsc">Past to Recent</option>
+              </select>
+            </div>
+
+            {/* Sort By Date */}
+            <div className="rounded-full bg-gray-50  shadow-sm shadow-gray-400 inline-block py-3 px-6 ">
+              <select className="bg-gray-50 focus:outline-none" value={sortByPrice} onChange={(e) => setSortByPrice(e.target.value)}>
+                <option value="" disabled>
+                  Sort By Date
+                </option>
+                <option value="asc">Cheap to Heigh</option>
+                <option value="dsc">Heigh to Cheap</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
-
       {/* Products */}
       <div className="lg:w-4/5 mx-auto mt-10 p-3 lg:mt-24">
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">

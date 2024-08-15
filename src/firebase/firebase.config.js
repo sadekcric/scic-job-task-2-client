3dfe587573903965d6baedc47e0de895f0569b18
sadekcric4 <sadekcric4@gmail.com>
2024-08-16 03:28:59 +0600

    firebase key security

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB15U_EfYIyTFoMDzMAXUTgSv9T5RUzXcQ",
-  authDomain: "scic-job-task-2.firebaseapp.com",
-  projectId: "scic-job-task-2",
-  storageBucket: "scic-job-task-2.appspot.com",
-  messagingSenderId: "991159598435",
-  appId: "1:991159598435:web:ed8cf6ac2fb90544166756",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase

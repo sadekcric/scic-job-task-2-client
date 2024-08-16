119e1111347a70b1e2d03d484e4a3d1dd4c8b1de
sadekcric4 <sadekcric4@gmail.com>
2024-08-16 20:16:32 +0600

    footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./component/Navbar/Navbar";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
